e78984168dd1910d66e9eaa95365a77e493cba49
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2022-09-24 16:16:12 +0200

    Set up endpoints and api calls

--- app/src/main/java/com/devwarex/currency/api/CurrencyService.kt
+++ app/src/main/java/com/devwarex/currency/api/CurrencyService.kt
@@ -1,6 +1,20 @@
 package com.devwarex.currency.api
 
+import com.devwarex.currency.util.EndPoints
+import retrofit2.http.GET
+
 interface CurrencyService {
 
 
+    @GET(EndPoints.ALL_CURRENCIES_RES)
+    suspend fun getAllCurrenciesSymbol()
+
+    @GET(EndPoints.CONVERT_CURRENCY_RES)
+    suspend fun currencyConversion()
+
+    @GET(EndPoints.TIME_SERIES_RES)
+    suspend fun getHistoricalCurrencyConversionRate()
+
+    @GET(EndPoints.LATEST_RATES_RES)
+    suspend fun getLatestCurrenciesRates()
 }
--- app/src/main/java/com/devwarex/currency/util/EndPoints.kt
+++ app/src/main/java/com/devwarex/currency/util/EndPoints.kt
@@ -2,5 +2,20 @@ package com.devwarex.currency.util
 
 object EndPoints {
 
-    const val BASE_URL = ""
+    const val BASE_URL = "https://api.apilayer.com/fixer/"
+    const val ALL_CURRENCIES_RES = "symbols"
+    const val CONVERT_CURRENCY_RES = "convert"
+    const val TIME_SERIES_RES = "timeseries"
+    const val LATEST_RATES_RES = "latest"
+
+    const val API_KEY_HEADER = "apikey"
+    const val TO_QUERY = "to"
+    const val FROM_QUERY = "from"
+    const val AMOUNT_QUERY = "amount"
+    const val START_DATE_QUERY = "start_date"
+    const val END_DATE_QUERY = "end_date"
+    const val BASE_CURRENCY_QUERY = "base"
+    const val SYMBOLS_QUERY = "symbols"
+
+
 }
