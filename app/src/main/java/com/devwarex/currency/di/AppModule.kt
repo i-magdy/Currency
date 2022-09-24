49037cde14cb2eb106c5bdea8d1aacff227753c7
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2022-09-24 20:32:08 +0200

    Create App Module DI

---
+++ app/src/main/java/com/devwarex/currency/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.devwarex.currency.di
+
+import android.content.Context
+import com.devwarex.currency.datastore.DatastoreImpl
+import com.devwarex.currency.db.AppDao
+import com.devwarex.currency.db.AppRoomDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun getInstanceRoomDBDao(@ApplicationContext context: Context): AppDao = AppRoomDatabase.getAppDatabase(context = context).appDao()
+
+    @Provides
+    @Singleton
+    fun getInstanceDatastore(@ApplicationContext context: Context): DatastoreImpl = DatastoreImpl.create(context = context)
+
+
+}
